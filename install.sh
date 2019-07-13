ec4b7766eb4a82e89a8a5f6b74af8a59112d5803
sarscoders <52663999+sarscoders@users.noreply.github.com>
2019-07-13 17:25:34 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+echo "develop by sarscoders"
+echo "###################"
+pkg update
+pkg upgrade
+echo "installing nano..."
+pkg install nano
+echo "installing clang..."
+pkg install clang
+echo "installing python..."
+pkg install python
+pkg install python2
+echo "installing php..."
+pkg install php
+echo "installing nmap..."
+pkg install nmap
+echo "###########"
+echo "thanks for using"
